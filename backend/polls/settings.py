94e4c12f4c9bdbcf083ebb0ac81e187f0c774ab5
Vaibhav Arora <vaibhavarora0301vishu@gmail.com>
2022-12-17 17:54:33 +0530

    cors setting updated

--- backend/polls/settings.py
+++ backend/polls/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
     'api',
     'user',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,4 +145,5 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://polls-app-two.vercel.app", "polls-app-vaibhav.vercel.app", "https://polls-app-git-master-vaibhav31.vercel.app"]
